f724d5c900d7d60052b2be3a17a8014dbf78fc56
Jordan <jordanjacobson2@gmail.com>
2020-03-06 16:27:36 +1100

    Added some functionality to line plot

--- thesis/Charter/PlotFolder/LineChart.java
+++ thesis/Charter/PlotFolder/LineChart.java
@@ -1,6 +1,9 @@
 package thesis.Charter.PlotFolder;
 
+import java.awt.Graphics2D;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 
 import thesis.Charter.Axis.Axis;
 import thesis.Charter.Axis.NumericAxis;
@@ -9,56 +12,162 @@ import thesis.Charter.Others.LineChartMeasurements;
 import thesis.DataFrame.DataFrame;
 import thesis.DataFrame.DataItem;
 
-public class LineChart extends XYChart{
+public class LineChart extends XYChart {
 
-	
 	NumericAxis axis;
 	LinePlot plot;
 	Legend legend;
-	
+
 	public LineChart(DataFrame dataFrame, String xAxis, String yAxis) {
 		super(dataFrame, dataFrame.GetColumnAsArray(xAxis), dataFrame.GetColumnAsArray(yAxis), "Bar");
-		
+
 		this.axis = new NumericAxis();
 		this.plot = new LinePlot();
 		this.legend = new Legend();
-		
+
 		cm = new LineChartMeasurements();
 
 	}
 
 	@Override
 	public void Create() {
-		String[] xValues = convertDataItemListToStringList(this.xData);
+		Number[] xValues = convertDataItemListIntoNumberList(this.xData);
+		Number[] yValues = convertDataItemListIntoNumberList(this.yData);
+
+		HashMap<Number, Number> data = calculateLineData(xValues, yValues);
+
+		Double minX = minimumXValue(data).doubleValue();
+		Double maxX = maximumXValue(data).doubleValue();
+		Double minY = minimumYValue(data).doubleValue();
+		Double maxY = maximumYValue(data).doubleValue();
 		
+		this.axis.calculateXAxis(minX, maxX);
+		this.axis.calculateYAxis(minY, maxY);
+
 		
+//		if (this.legend.getIncludeLegend()) {
+//			Object[] hueValies = uniqueColorCodeValues;
+//			this.legend.calculateLegend(this.colorCodeLabel, hueValies);
+//		}
+
+		cm.calculateChartImageMetrics(this.axis, this.plot, this.legend, getTitle(), getTitleFont());
+
+		instantiateChart(cm);
+
+		Graphics2D g = initializaGraphicsObject(cm);
+		drawBackground(g, cm);
+
+		this.plot.drawChartBackground(g, cm);
 		
+		this.axis.drawAxis(g, cm);
 		
-//		this.axis.calculateXAxis(0, 10);
+		this.plot.drawPlotOutline(g, cm);
+		
+		this.axis.drawAxisTicks(g, cm);
+		
+		this.plot.drawPlot(g, this.axis, data, cm);
+	}
+
+	private Number minimumXValue(HashMap<Number, Number> data) {
+		Number min = null;
+
+		for (Number xValue : data.keySet()) {
+			if ((min == null) || (xValue.doubleValue() < min.doubleValue())) {
+				min = xValue;
+			}
+		}
+
+		return min;
+
+	}
+
+	private Number maximumXValue(HashMap<Number, Number> data) {
+		Number max = null;
+
+		for (Number xValue : data.keySet()) {
+			if ((max == null) || (xValue.doubleValue() > max.doubleValue())) {
+				max = xValue;
+			}
+		}
+
+		return max;
+
 	}
 	
+	private Number minimumYValue(HashMap<Number, Number> data) { 
+		Number min = null;
+		for (Number xValue: data.keySet()) {
+			if ((min == null) || (data.get(xValue).doubleValue() < min.doubleValue())) {
+				min = data.get(xValue);
+			}
+		}
+		
+		return min;
+	}
+
+	private Number maximumYValue(HashMap<Number, Number> data) { 
+		Number max = null;
+		for (Number xValue: data.keySet()) {
+			if ((max == null) || (data.get(xValue).doubleValue() > max.doubleValue())) {
+				max = data.get(xValue);
+			}
+		}
+		
+		return max;
+	}
+	
+	private HashMap<Number, Number> calculateLineData(Number[] xValues, Number[] yValues) {
+		Number[] uniqueXValues = removeDuplicates(xValues);
+		HashMap<Number, Number> runningTotals = new HashMap<Number, Number>();
+		HashMap<Number, Number> runningCount = new HashMap<Number, Number>();
+
+		for (Number xValue : uniqueXValues) {
+			runningTotals.put(xValue, 0);
+			runningCount.put(xValue, 0);
+		}
+
+		for (int i = 0; i < xValues.length; i++) {
+			runningTotals.put(xValues[i], runningTotals.get(xValues[i]).doubleValue() + yValues[i].doubleValue());
+			runningCount.put(xValues[i], runningCount.get(xValues[i]).intValue() + 1);
+		}
+
+		HashMap<Number, Number> data = new HashMap<Number, Number>();
+		for (Number xValue : uniqueXValues) {
+			data.put(xValue, runningTotals.get(xValue).doubleValue() / runningCount.get(xValue).doubleValue());
+		}
+
+		return data;
+	}
+
+	private Number[] convertDataItemListIntoNumberList(DataItem[] list) {
+		Number[] numberList = new Number[list.length];
+		for (int i = 0; i < list.length; i++) {
+			numberList[i] = list[i].getValueConvertedToNumber();
+		}
+
+		return numberList;
+	}
+
 	private String[] convertDataItemListToStringList(DataItem[] list) {
 		String[] stringList = new String[list.length];
 		for (int i = 0; i < list.length; i++) {
 			stringList[i] = list[i].getValueConvertedToString();
 		}
-		
+
 		return stringList;
 	}
 
-	private String[] getXDataOrdered() {
-		ArrayList<String> foundXCatagories = new ArrayList<String>();
-		for (DataItem xValue : this.xData) {
-			if (!foundXCatagories.contains(xValue.getValueConvertedToString())) {
-				foundXCatagories.add(xValue.getValueConvertedToString());
+	private Number[] removeDuplicates(Number[] list) {
+		ArrayList<Number> newList = new ArrayList<Number>();
+		for (int i = 0; i < list.length; i++) {
+			if (!newList.contains(list[i])) {
+				newList.add(list[i]);
 			}
 		}
-		
-		String[] xDataFormatted = new String[foundXCatagories.size()];
-		xDataFormatted = foundXCatagories.toArray(xDataFormatted);
-		return xDataFormatted;
+		return newList.toArray(new Number[0]);
+
 	}
-	
+
 	public Axis getAxis() {
 		return this.axis;
 	}
--- thesis/Charter/PlotFolder/LinePlot.java
+++ thesis/Charter/PlotFolder/LinePlot.java
@@ -1,5 +1,26 @@
 package thesis.Charter.PlotFolder;
 
-public class LinePlot {
+import java.awt.Graphics2D;
+import java.util.HashMap;
+
+import thesis.Charter.Axis.Axis;
+import thesis.Charter.Axis.NumericAxis;
+import thesis.Charter.Others.XYChartMeasurements;
+import thesis.DataFrame.DataItem;
+
+public class LinePlot extends Plot{
+
+	public void drawPlot(Graphics2D g, NumericAxis axis, HashMap<Number, Number> data, XYChartMeasurements cm) {
+
+		System.out.println("use this");
+	}
+
+	@Override
+	public void drawPlot(Graphics2D g, Axis axis, DataItem[] xData, DataItem[] yData, Object[] colorCodeValues,
+			XYChartMeasurements cm) {
+		// TODO Auto-generated method stub
+		
+	}
+
 
 }
