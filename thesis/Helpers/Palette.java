5b1921bdfcb0a47a80dd8d33243b0d81955865dc
Jordan <jordanjacobson2@gmail.com>
2020-03-16 13:14:27 +1100

    Added color generater

--- thesis/Helpers/Palette.java
+++ thesis/Helpers/Palette.java
@@ -10,9 +10,20 @@ public class Palette {
 	public final static Color[] Fire = new Color[] {Color.decode("#BC3908"), Color.decode("#941B0C"), Color.decode("#F6AA1C"), Color.decode("#220901"), Color.decode("#621708")};
 	public final static Color[] Contrast = new Color[] {Color.decode("#c80003"), Color.decode("#a6c64c"), Color.decode("#ff955f"), Color.decode("#405d3a"), Color.decode("#86c6be")};
 
-	public final static Color[] Default = new Color[] {Color.BLUE, Color.CYAN, Color.RED, Color.GREEN, Color.MAGENTA, Color.ORANGE, Color.PINK, Color.BLACK};
+	public final static Color[] Default = new Color[] {new Color(60, 92, 160), new Color(211, 112, 65), new Color(70, 155, 85), new Color(181, 57, 65), new Color(109, 92, 163), new Color(128, 101, 78), new Color(208, 116, 182), new Color(121, 121, 121), new Color(192, 172, 97), new Color(84, 166, 194)};
 	
 	
-//	public final static Color[] a = new Color[] {Color.decode("#"), Color.decode("#"), Color.decode("#"), Color.decode("#"), Color.decode("#")};
 
+
+	public static Color[] generateUniqueColors(int numColors) {
+		Color[] colors = new Color[numColors];
+		
+		for (int colorCount = 0; colorCount < numColors; colorCount++) {
+			colors[colorCount] = Color.getHSBColor((float)colorCount / (float)numColors, 1, 1);
+			
+		}
+		
+		return colors;
+	}
+	
 }
